22187fdaad78c23ed7ae45eb68aca4ed85cd6353
roido_ross <code.with.lloyd@gmail.com>
2025-01-20 10:07:59 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -10,8 +10,6 @@ const nextConfig = {
   // Add any other Next.js config options you need
 }
 
-<<<<<<< HEAD
+
 module.exports = withBundleAnalyzer(nextConfig);
-=======
-module.exports = withBundleAnalyzer(nextConfig);
->>>>>>> 6e45bbe71281a9c3acad20ba7c0a6fa57e6e4e7a
+
