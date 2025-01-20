6e45bbe71281a9c3acad20ba7c0a6fa57e6e4e7a
roido_ross <code.with.lloyd@gmail.com>
2025-01-20 09:50:13 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 const bundleAnalyzer = require('@next/bundle-analyzer');
+
 const withBundleAnalyzer = bundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // your config options here
+  reactStrictMode: true,
+  // Add any other Next.js config options you need
 }
 
 module.exports = withBundleAnalyzer(nextConfig);
